046282d94752469c4af2e035af2efcc75997ac1e
xiaogavin-hunter <gavin.xiao89@myhunter.cuny.edu>
2022-02-14 16:38:12 -0500

    Added printing of board and inputting of board

--- assignments/recursive-search/main.cpp
+++ assignments/recursive-search/main.cpp
@@ -7,3 +7,35 @@ char knight = 'N';
 char wall = '#';
 char empty = ' ';
 char visited = '.';
+
+int input_board(std::string filename, char board[11][11]) { 
+    std::fstream filein(filename);
+    std::string temp; 
+    int counter = 0; 
+    while(std::getline(filein, temp)) { 
+        for(int i = 0; i < temp.length(); i++) { 
+            board[counter][i] = temp[i];
+        }
+
+        counter++;
+    }
+
+    return counter;
+}
+
+void print_board(char board[11][11], int size) { 
+    for(int i = 0; i < size; i++) { 
+        for(int j = 0; j < size; j++) { 
+            std::cout << board[i][j];
+        }
+
+        std::cout << std::endl;
+    }
+}
+
+int main() { 
+    char board[11][11]; 
+    int size = input_board("board.dat", board);
+    print_board(board, size); 
+    return 0; 
+}

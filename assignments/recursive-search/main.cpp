16a13c681efe60c41b7573328acb54c08c459a50
xiaogavin-hunter <gavin.xiao89@myhunter.cuny.edu>
2022-02-17 09:46:06 -0500

    fix output

--- assignments/recursive-search/main.cpp
+++ assignments/recursive-search/main.cpp
@@ -26,15 +26,19 @@ int input_board(std::string filename, char board[11][11]) {
 void print_board(char board[11][11], int size) {
     std::cout << "[0;0H\n"; 
     for(int i = 0; i < size; i++) { 
-        for(int j = 0; j < size; j++) { 
-            std::cout << board[i][j];
+        for(int j = 0; j < size; j++) {
+            if(board[i][j] != wall) {
+                std::cout << ":" << board[i][j];
+            } else { 
+                std::cout << " ";
+            }
         }
 
-        std::cout << std::endl;
+        std::cout << ":" << std::endl;
     }
 }
 
-void solve(char board[11][11], int row, int col, int size) { 
+void solve(char board[11][11], int row, int col, int size, int counter) { 
     if(board[row][col] == wall || board[row][col] == knight) { 
         return;
     }
@@ -43,20 +47,23 @@ void solve(char board[11][11], int row, int col, int size) {
     usleep(80000);
     print_board(board, size);
 
-    solve(board, row - 2, col + 1, size); 
-    solve(board, row - 1, col + 2, size);
-    solve(board, row + 1, col + 2, size);
-    solve(board, row + 2, col + 1, size);
-    solve(board, row + 2, col - 1, size);
-    solve(board, row + 1, col - 2, size);
-    solve(board, row - 1, col - 2, size);
-    solve(board, row - 2, col - 1, size);
+    //4 * row + col
+
+    solve(board, row - 2, col + 1, size, counter); 
+    solve(board, row - 1, col + 2, size, counter);
+    solve(board, row + 1, col + 2, size, counter);
+    solve(board, row + 2, col + 1, size, counter);
+    solve(board, row + 2, col - 1, size, counter);
+    solve(board, row + 1, col - 2, size, counter);
+    solve(board, row - 1, col - 2, size, counter);
+    solve(board, row - 2, col - 1, size, counter);
 }
 
 int main() { 
     char board[11][11]; 
     int size = input_board("board.dat", board);
+    std::cout << "[2J;\n";
     print_board(board, size); 
-    solve(board, 2, 2, size);
+    solve(board, 2, 2, size, 0);
     return 0; 
 }

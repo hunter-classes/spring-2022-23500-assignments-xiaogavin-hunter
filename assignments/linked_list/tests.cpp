5adcdb67419906ffa448a5086acbe3fb2d636493
xiaogavin-hunter <gavin.xiao89@myhunter.cuny.edu>
2022-03-06 13:02:49 -0500

    Finished testing

--- assignments/linked_list/tests.cpp
+++ assignments/linked_list/tests.cpp
@@ -192,6 +192,25 @@ TEST_CASE("Get") {
         CHECK(list->get(6) == -1);
         delete list;
     }
+
+    SUBCASE("Reversed list") {
+        OList *list = new OList();
+        list->insert(4);
+        list->insert(6);
+        list->insert(1);
+        list->insert(8);
+        list->insert(14);
+        list->insert(268);
+        list->reverse();
+
+        CHECK(list->get(0) == 268);
+        CHECK(list->get(1) == 14);
+        CHECK(list->get(2) == 8);
+        CHECK(list->get(3) == 6);
+        CHECK(list->get(4) == 4);
+        CHECK(list->get(5) == 1);
+        delete list;
+    }
 }
 
 TEST_CASE("Remove") {
@@ -203,19 +222,119 @@ TEST_CASE("Remove") {
         delete list;
     }
 
-    SUBCASE("Test one, empty list") {
+    SUBCASE("Test two, removing four elements") {
         OList *list = new OList();
+        list->insert(120);
+        list->insert(5919);
+        list->insert(1);
+        list->insert(8);
+        list->insert(14);
+        list->insert(268);
 
+        CHECK(list->toString() == "head ---> 1 ---> 8 ---> 14 ---> 120 ---> 268 ---> 5919 ---> null");
 
+        list->remove(1);
+        CHECK(list->toString() == "head ---> 1 ---> 14 ---> 120 ---> 268 ---> 5919 ---> null");
+        list->remove(1);
+        CHECK(list->toString() == "head ---> 1 ---> 120 ---> 268 ---> 5919 ---> null");
+        list->remove(1);
+        CHECK(list->toString() == "head ---> 1 ---> 268 ---> 5919 ---> null");
+        list->remove(1);
+        CHECK(list->toString() == "head ---> 1 ---> 5919 ---> null");
+        
+        CHECK_THROWS_AS(list->remove(-1), std::out_of_range);
         delete list;
     }
 
-    SUBCASE("Test one, empty list") {
+    SUBCASE("Test three, reversed list") {
         OList *list = new OList();
+        list->insert(14);
+        list->insert(1411);
+        list->insert(-5000);
+        list->insert(8);
+        list->insert(14);
+        list->insert(2222);
+        list->reverse();
 
+        CHECK(list->toString() == "head ---> 2222 ---> 1411 ---> 14 ---> 14 ---> 8 ---> -5000 ---> null");
+
+        list->remove(0);
+        CHECK(list->toString() == "head ---> 1411 ---> 14 ---> 14 ---> 8 ---> -5000 ---> null");
+        list->remove(1);
+        CHECK(list->toString() == "head ---> 1411 ---> 14 ---> 8 ---> -5000 ---> null");
+        list->remove(2);
+        CHECK(list->toString() == "head ---> 1411 ---> 14 ---> -5000 ---> null");
+
+        list->reverse();
+        CHECK(list->toString() == "head ---> -5000 ---> 14 ---> 1411 ---> null");
+        list->remove(2);
+        CHECK(list->toString() == "head ---> -5000 ---> 14 ---> null");
+        CHECK_THROWS_AS(list->remove(2), std::out_of_range);
 
         delete list;
     }
-
 }
 
+TEST_CASE("Reverse") {
+    SUBCASE("Test one, empty list") { 
+        OList *list = new OList(); 
+        // list->reverse();
+
+        CHECK_THROWS_AS(list->reverse(), std::out_of_range);
+        delete list;
+    }
+
+    SUBCASE("Test two, list with one element") { 
+        OList *list = new OList(); 
+        list->insert(1);
+        // list->reverse();
+
+        CHECK_THROWS_AS(list->reverse(), std::out_of_range);
+        delete list;
+    }
+
+    SUBCASE("Test three, list with two elements") { 
+        OList *list = new OList(); 
+        list->insert(1);
+        list->insert(2);
+        CHECK(list->toString() == "head ---> 1 ---> 2 ---> null");
+
+        list->reverse();
+        CHECK(list->toString() == "head ---> 2 ---> 1 ---> null");
+        delete list;
+    }
+
+    SUBCASE("Test four, list with repeating elements") {
+        OList *list = new OList(); 
+        list->insert(1);
+        list->insert(2);
+        list->insert(1);
+        list->insert(2);
+        CHECK(list->toString() == "head ---> 1 ---> 1 ---> 2 ---> 2 ---> null");
+
+        list->reverse();
+        CHECK(list->toString() == "head ---> 2 ---> 2 ---> 1 ---> 1 ---> null");
+        delete list;
+    }
+
+    SUBCASE("Test five, list multiple elements, reversed twice") {
+        OList *list = new OList(); 
+        list->insert(1);
+        list->insert(2);
+        list->insert(1);
+        list->insert(2);
+        list->insert(42);
+        list->insert(555);
+        list->insert(-18181);
+        list->insert(15125);
+        list->insert(55);
+        list->insert(15);
+        list->insert(6);
+        list->insert(2882);
+        CHECK(list->toString() == "head ---> -18181 ---> 1 ---> 1 ---> 2 ---> 2 ---> 6 ---> 15 ---> 42 ---> 55 ---> 555 ---> 2882 ---> 15125 ---> null");
+
+        list->reverse();
+        CHECK(list->toString() == "head ---> 15125 ---> 2882 ---> 555 ---> 55 ---> 42 ---> 15 ---> 6 ---> 2 ---> 2 ---> 1 ---> 1 ---> -18181 ---> null");
+        delete list;
+    }
+}

e0dfd194b8072f9bef486071405470fe25a2d7f6
xiaogavin-hunter <gavin.xiao89@myhunter.cuny.edu>
2022-03-06 13:02:43 -0500

    added examples

--- assignments/linked_list/main.cpp
+++ assignments/linked_list/main.cpp
@@ -10,50 +10,40 @@
 
 int main(int argc, char *argv[]) {
 
-    // OList *list = new OList();
-
-    // // list->insert(1);
-    // list->insert(-1);
-    // // list->insert(0);
-    // list->insert(-5);
-    // list->insert(4);
-    // list->insert(2);
-    // list->insert(10);
-    // list->insert(124);
-    // list->insert(1);
-    // list->insert(6);
-    // list->insert(-4);
-    // list->insert(-1);
-
-    // std::cout << list->toString() << std::endl;
-
-    // std::string tmp = list->contains(1) ? "true" : "false";
-    // std::cout << "list contains 1: " << tmp << std::endl;
-    // tmp = list->contains(1000) ? "true" : "false";
-    // std::cout << "list contains 1000: " << tmp << std::endl;
-
-    // std::cout << list->get(11) << std::endl;
-
-    // list->reverse();
-    // std::cout << list->toString() << std::endl;
-
-    // list->insert(14124);
-    // list->insert(5);
-    // list->insert(-5);
-    // std::cout << list->toString() << std::endl;
-    
-    // list->reverse();
-    // list->remove(1);
-    // std::cout << list->toString() << std::endl;
-
     OList *list = new OList();
-        list->insert(100);
-        list->insert(101);
-        list->insert(100);
-        list->insert(500);
-        list->insert(100);
-        list->insert(1);
-        list->insert(0);
-        std::cout << list->toString() << std::endl;
+
+    list->insert(1);
+    list->insert(-1);
+    list->insert(0);
+    list->insert(-5);
+    list->insert(4);
+    list->insert(2);
+    list->insert(10);
+    list->insert(124);
+    list->insert(1);
+    list->insert(6);
+    list->insert(-4);
+    list->insert(-1);
+
+    std::cout << list->toString() << std::endl;
+
+    std::string tmp = list->contains(1) ? "true" : "false";
+    std::cout << "list contains 1: " << tmp << std::endl;
+    tmp = list->contains(1000) ? "true" : "false";
+    std::cout << "list contains 1000: " << tmp << std::endl;
+
+    std::cout << list->get(11) << std::endl;
+
+    list->reverse();
+    std::cout << list->toString() << std::endl;
+
+    list->insert(14124);
+    list->insert(5);
+    list->insert(-5);
+    std::cout << list->toString() << std::endl;
+    
+    list->reverse();
+    list->remove(1);
+    std::cout << list->toString() << std::endl;
     return 0;
 }

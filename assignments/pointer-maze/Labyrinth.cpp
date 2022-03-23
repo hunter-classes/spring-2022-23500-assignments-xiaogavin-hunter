50a5ed337d4fd3fc4db85f849ce9139ed1b2f451
xiaogavin-hunter <gavin.xiao89@myhunter.cuny.edu>
2022-03-23 13:24:14 -0400

    added code for isPathToFreedom

--- assignments/pointer-maze/Labyrinth.cpp
+++ assignments/pointer-maze/Labyrinth.cpp
@@ -1,8 +1,50 @@
 #include "Labyrinth.h"
 
+
+
 bool isPathToFreedom(MazeCell* start, const std::string& moves) {
     /* TODO: Delete this comment and the remaining lines, then implement this function. */
-    (void) start;
-    (void) moves;
+    bool found_spell = false;
+    bool found_potion = false;
+    bool found_wand = false;    
+
+    for(int i = 0; i < moves.length(); i++) {
+        switch(moves[i]) { 
+            case 'N': 
+                start = start->north;
+                break;
+            case 'E':
+                start = start->east;
+                break;
+            case 'S':
+                start = start->south;
+                break;
+            case 'W':
+                start = start->west;
+                break;
+        }
+
+        if(start->whatsHere != Item::NOTHING) { 
+            switch(start->whatsHere) { 
+                case Item::POTION:
+                    found_potion = true;
+                    break;
+                case Item::SPELLBOOK:
+                    found_spell = true;
+                    break;
+                case Item::WAND:
+                    found_wand = true;
+                    break;
+            }
+        } 
+    }
+
+    if(found_potion && found_spell && found_wand) { 
+        return true;
+    } 
+
     return false;
+    
 }
+
+// char item()

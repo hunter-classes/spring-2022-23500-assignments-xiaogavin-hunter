f6975797eea7376945445ec29fba8666bc1628e0
xiaogavin-hunter <gavin.xiao89@myhunter.cuny.edu>
2022-04-08 10:18:04 -0400

    Initial commit

---
+++ assignments/stacks_queues/List.h
@@ -0,0 +1,17 @@
+#pragma once 
+
+#include "Node.h"
+
+class List { 
+    private: 
+        Node *head;
+
+    public: 
+        List();
+        ~List(); 
+        void insert(std::string data);
+        void insert(int loc, std::string data); 
+        std::string toString(); 
+        int locate(std::string data); 
+        void remove(int location);
+};
---
+++ assignments/stacks_queues/stack.h
@@ -0,0 +1,33 @@
+#pragma once 
+
+#include "List.h"
+
+class Stack { 
+
+    /* Stack is LAST IN FIRST OUT 
+     * // Constructor/destructors
+     * push(int) // (you can change the type to std::string if you'd like)
+     * int pop() // remove and return the top item
+     * int top() // return but do not remove the top item
+     * bool is_empty() // return true if empty, false otherwise
+     */
+
+    private: 
+        int items; 
+        List *head; 
+
+    public:
+        // pushes item onto Stack
+        push(std::string item);
+
+        // removes and returns top item 
+        std::string pop();
+
+        // returns top item 
+        std::string top();
+
+        // check to see if the list is empty 
+        bool is_empty();
+
+
+}

4f062af4f8283361f9cebd3618bba3bbbd962234
xiaogavin-hunter <gavin.xiao89@myhunter.cuny.edu>
2022-04-14 10:29:50 -0400

    need to fix enqueue/dequeue

--- assignments/stacks_queues/queue.cpp
+++ assignments/stacks_queues/queue.cpp
@@ -2,15 +2,37 @@
 #include <iostream>
 #include "queue.h"
 
+#define ERR_QUEUE_EMPTY 1
+#define ERR_QUEUE_FULL 2
+#define ERR_QUEUE_OTHER 4 
+
 Queue::Queue() { 
     this->arr = new int[SIZE];
     this->start = 0;
-    this->end = SIZE;
+    this->end = 0;
 }
 
 std::string Queue::print_debug_str() { 
     std::string tmp = "";
-    tmp += "Start # of arr: " + std::to_string(this->start) + " | End of arr: " + std::to_string(this->end);
+    tmp += "Start of arr: " + std::to_string(this->start) + " | End of arr: " + std::to_string(this->end) + "\n";
+
+    for(int i = start; i < end; i++) { 
+        tmp += std::to_string(arr[i]) + " ";
+    }
 
     return tmp;
+}
+
+void Queue::enqueue(int a) { 
+    if(end - start + 1 > SIZE) { 
+        throw ERR_QUEUE_FULL;
+    }
+
+    arr[end] = a;
+    end++;
+}
+
+int Queue::dequeue() {
+    start++;
+    return arr[start - 1];
 }

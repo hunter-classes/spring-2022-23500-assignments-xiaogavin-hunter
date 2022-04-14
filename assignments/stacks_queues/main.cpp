c772caa84f3181a35146e3a48f7f2c6f3572575f
xiaogavin-hunter <gavin.xiao89@myhunter.cuny.edu>
2022-04-14 09:24:01 -0400

    edited

--- assignments/stacks_queues/main.cpp
+++ assignments/stacks_queues/main.cpp
@@ -2,43 +2,49 @@
 #include <string>
 
 #include "stack.h"
+#include "queue.h"
 
 int main() { 
-    Stack *stk = new Stack();
-    stk->push("one");
-    stk->push("two");
-    stk->push("three");
+    // Stack *stk = new Stack();
+    // stk->push("one");
+    // stk->push("two");
+    // stk->push("three");
 
-    std::cout << stk->print() << std::endl;
-    std::cout << stk->top() << std::endl;
-    stk->pop();
+    // std::cout << stk->print() << std::endl;
 
-    std::cout << stk->top() << std::endl;
+    // std::cout << stk->print() << std::endl;
+    // std::cout << stk->top() << std::endl;
+    // stk->pop();
 
-    try { 
-        stk->pop();
-    } catch(int e) { 
-        std::cout << "Empty stack" << std::endl;
-    }
+    // std::cout << stk->top() << std::endl;
 
-    try { 
-        stk->pop();
-    } catch(int e) { 
-        std::cout << "Empty stack" << std::endl;
-    }
+    // try { 
+    //     stk->pop();
+    // } catch(int e) { 
+    //     std::cout << "Empty stack" << std::endl;
+    // }
 
-    try { 
-        stk->pop();
-    } catch(int e) { 
-        std::cout << "Empty stack" << std::endl;
-    }
+    // try { 
+    //     stk->pop();
+    // } catch(int e) { 
+    //     std::cout << "Empty stack" << std::endl;
+    // }
+
+    // try { 
+    //     stk->pop();
+    // } catch(int e) { 
+    //     std::cout << "Empty stack" << std::endl;
+    // }
 
 
-    std::cout << stk->is_empty() << std::endl;
+    // std::cout << stk->is_empty() << std::endl;
 
-    stk->push("item");
+    // stk->push("item");
 
-    std::cout << stk->is_empty() << std::endl;
+    // std::cout << stk->is_empty() << std::endl;
 
+    Queue *queue = new Queue(); 
+
+    std::cout << queue->print_debug_str() << std::endl;
     return 0; 
 }

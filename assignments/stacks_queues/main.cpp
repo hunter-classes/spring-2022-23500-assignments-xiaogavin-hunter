d9b688c38a3cc3dc331bd550a35ac370d5bcad6a
xiaogavin-hunter <gavin.xiao89@myhunter.cuny.edu>
2022-04-24 13:32:10 -0400

    Added examples

--- assignments/stacks_queues/main.cpp
+++ assignments/stacks_queues/main.cpp
@@ -4,65 +4,83 @@
 #include "stack.h"
 #include "queue.h"
 
-int main() { 
-    Queue *queue = new Queue(); 
-    std::cout << queue->print_debug_str() << std::endl;
-    
-    for(int i = 0; i <= 5; i++) { 
-        queue->enqueue(i);
-        std::cout << queue->print_debug_str() << std::endl;
+int main() {
+    Stack *s = new Stack();
+    std::cout << "Stack should be empty: " << s->is_empty() << std::endl;
+    std::cout << "==============================================" << std::endl;
+
+    s->push("one"); 
+    s->push("two");
+    s->push("three");
+    std::cout << "Stack should be three-->two-->one: " << s->print() << std::endl;
+    std::cout << "==============================================" << std::endl;
+
+    std::cout << "Top of stack should be three: " << s->top() << std::endl;
+    std::cout << "==============================================" << std::endl;
+
+    std::cout << "Removing all items from stack: " << std::endl;
+    for(int i = 0; i < 3; i++) {
+        std::cout << "Pushing items from stack: " << s->pop() << std::endl;;
     }
+    std::cout << "==============================================" << std::endl;
 
-    for(int i = 0; i < 4; i++) { 
-        try {
-            queue->enqueue(1);
-            std::cout << queue->print_debug_str() << std::endl;
-        } catch(int e) { 
-            std::cout << "Queue full: Error code: " + std::to_string(e) << std::endl;
-        }
-    }
-    
-    
-
-    std::cout << "Dequeue val: " << queue->dequeue() << std::endl;
-    std::cout << queue->print_debug_str() << std::endl;
-    std::cout << "Dequeue val: " << queue->dequeue() << std::endl; 
-    std::cout << queue->print_debug_str() << std::endl;
-    std::cout << "Dequeue val: " << queue->dequeue() << std::endl; 
-    std::cout << queue->print_debug_str() << std::endl;
-    std::cout << "Dequeue val: " << queue->dequeue() << std::endl; 
-    std::cout << queue->print_debug_str() << std::endl;
-    std::cout << "Dequeue val: " << queue->dequeue() << std::endl; 
-    std::cout << queue->print_debug_str() << std::endl;
-
-    try {
-        std::cout << "Dequeue val: " << queue->dequeue() << std::endl; 
-        std::cout << queue->print_debug_str() << std::endl;
+    std::cout << "Trying to pop item from empty stack" << std::endl; 
+    try { 
+        s->pop();
     } catch(int e) { 
-        std::cout << "Queue empty: Error code: " + std::to_string(e) << std::endl;
+        std::cout << "Empty stack, Error code: " << std::to_string(e) << std::endl;
     }
-
-    try {
-        std::cout << "Dequeue val: " << queue->dequeue() << std::endl; 
-        std::cout << queue->print_debug_str() << std::endl;
-    } catch(int e) { 
-        std::cout << "Queue empty: Error code: " + std::to_string(e) << std::endl;
-    }
-
-    try {
-        std::cout << "Dequeue val: " << queue->dequeue() << std::endl; 
-        std::cout << queue->print_debug_str() << std::endl;
-    } catch(int e) { 
-        std::cout << "Queue empty: Error code: " + std::to_string(e) << std::endl;
-    }
-
-    try {
-        std::cout << "Dequeue val: " << queue->dequeue() << std::endl; 
-        std::cout << queue->print_debug_str() << std::endl;
-    } catch(int e) { 
-        std::cout << "Queue empty: Error code: " + std::to_string(e) << std::endl;
-    }
-
     
+    std::cout << "==============================================" << std::endl << std::endl;
+
+    Queue *q = new Queue(); 
+    std::cout << q->print_debug_str() << std::endl;
+    std::cout << "Queue is empty: " << q->is_empty() << std::endl; 
+    std::cout << "Queue is full: " << q->is_full() << std::endl;
+    std::cout << "==============================================" << std::endl;
+
+    q->enqueue(1);
+    q->enqueue(2);
+    q->enqueue(3);
+    std::cout << "Should be 1, 2, 3: " << q->print_debug_str() << std::endl;
+    std::cout << "Dequeue one item: " << q->dequeue() << std::endl;
+    std::cout << q->print_debug_str() << std::endl;
+    std::cout << "==============================================" << std::endl;
+
+    for(int i = 4; i <= 9; i++) {
+        std::cout << "Enqueueing: " << std::to_string(i) << std::endl; 
+        q->enqueue(i);
+    }
+
+    std::cout << "Filling up queue: " << q->print_debug_str() << std::endl;
+    std::cout << "Queue is empty: " << q->is_empty() << std::endl; 
+    std::cout << "Queue is full: " << q->is_full() << std::endl;
+
+    std::cout << "Trying to add one item onto full queue: ";
+    try { 
+        q->enqueue(10);
+    } catch(int e) {
+        std::cout << "\nQueue is full: Error code: " << std::to_string(e) << std::endl;
+    }
+    std::cout << "==============================================" << std::endl;
+
+    std::cout << "Emptying queue: " << std::endl;
+
+    for(int i = 0; i < 8; i++) { 
+        std::cout << "Dequeueing:" << q->dequeue() << '\n'; 
+    }
+
+    std::cout << "Queue right now: " << q->print_debug_str() << std::endl;
+    std::cout << "Queue is empty: " << q->is_empty() << std::endl; 
+    std::cout << "Queue is full: " << q->is_full() << std::endl;
+
+    try { 
+        std::cout << "Dequeue: " << q->dequeue() << std::endl;
+    } catch(int e) {
+        std::cout << "\nQueue is empty: Error code: " << std::to_string(e) << std::endl;
+    }
+    std::cout << "==============================================" << std::endl;
+
+
     return 0; 
 }

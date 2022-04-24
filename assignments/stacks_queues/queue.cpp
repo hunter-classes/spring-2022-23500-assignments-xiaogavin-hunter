4619be59c2a91ce5649bf405e19ba653496dd64e
xiaogavin-hunter <gavin.xiao89@myhunter.cuny.edu>
2022-04-24 13:32:04 -0400

    fixed enqueue and dequeue

--- assignments/stacks_queues/queue.cpp
+++ assignments/stacks_queues/queue.cpp
@@ -8,36 +8,86 @@
 
 Queue::Queue() { 
     this->arr = new int[SIZE];
+    this->start = 0;
     this->end = 0;
+    this->total_items = 0;  
 }
 
-std::string Queue::print_debug_str() { 
-    std::string tmp = "";
-    tmp += "Start of arr: " + std::to_string(this->start) + " | End of arr: " + std::to_string(this->end) + "\n";
-
-    for(int i = 0; i < end; i++) { 
-        tmp += std::to_string(arr[i]) + " ";
-    }
-
-    return tmp;
+Queue::~Queue() { 
+    delete[] this->arr;
+    this->start = 0;
+    this->end = 0;
+    this->total_items = 0;
 }
 
-void Queue::enqueue(int a) { 
-    if(end == SIZE) { 
+bool Queue::is_empty() {
+    return start == end && total_items < 1;  
+}
+
+bool Queue::is_full() {
+    return start == end && total_items > 0 ; 
+}
+
+void Queue::enqueue(int i) { 
+    if(is_full()) { 
         throw ERR_QUEUE_FULL;
     }
 
-    arr[end] = a;
-    end++;
+    arr[end] = i; 
+    end++; 
+    total_items++; 
+
+    if(end >= SIZE) { 
+        end %= SIZE; 
+    }
+
 }
 
-int Queue::dequeue() {
-    if(end == 0) {
+int Queue::dequeue() { 
+    if(is_empty()) { 
+        throw ERR_QUEUE_EMPTY; 
+    }
+
+    int tmp = arr[start];
+    start++;
+    total_items--;
+
+    if(start >= SIZE) { 
+        start %= SIZE;
+    }
+
+    return tmp; 
+}
+
+int Queue::front() { 
+    if(is_empty()) { 
         throw ERR_QUEUE_EMPTY;
     }
 
-    int rtn = arr[end - 1]; 
-    end--;
+    return arr[start];
+}
 
-    return rtn;
+std::string Queue::print_debug_str() { 
+    std::string tmp = "start-->"; 
+    
+    if(!is_empty()) { 
+        if(start < end) {
+            for(int i = start; i < end; i++) { 
+                tmp = tmp + std::to_string(arr[i]) + "-->";
+            }
+        } else { 
+            for(int i = start; i < SIZE; i++) {
+                tmp = tmp + std::to_string(arr[i]) + "-->";
+            }
+
+            for(int j = 0; j < end; j++) {
+                tmp = tmp + std::to_string(arr[j]) + "-->";
+            }
+        }
+    }
+    
+
+    tmp = tmp + "end";
+
+    return tmp; 
 }

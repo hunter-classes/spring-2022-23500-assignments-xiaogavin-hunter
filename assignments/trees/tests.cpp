cb15aa31e23326f25ca89f7a5a6bfb4130bf0563
xiaogavin-hunter <gavin.xiao89@myhunter.cuny.edu>
2022-05-01 13:25:27 -0400

    added tests for rsearch

--- assignments/trees/tests.cpp
+++ assignments/trees/tests.cpp
@@ -3,3 +3,20 @@
 #include "doctest.h"
 #include "BSTree.h"
 
+TEST_CASE("Checking to see debug_string") {
+    SUBCASE("Setup from classcode: ") { 
+        BSTree *t = new BSTree();
+        t->setup();
+        CHECK(t->get_debug_string() == "3-->5-->7-->10-->15-->20-->30-->nullptr");
+        delete t; 
+    }
+
+    SUBCASE("Checking resursive search") { 
+        BSTree *t = new BSTree();
+        t->setup();
+
+        CHECK(t->rsearch(3) == t->search(3));
+        CHECK(t->rsearch(5) == t->search(5));
+        CHECK_THROWS(t->rsearch(155555));
+    }
+}

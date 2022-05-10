1c618e0742f9ae36f76fbdd1123e6d0e83fa314c
xiaogavin-hunter <gavin.xiao89@myhunter.cuny.edu>
2022-05-10 17:05:01 -0400

    Added task 3

--- assignments/trees/BSTree.h
+++ assignments/trees/BSTree.h
@@ -8,7 +8,9 @@ class BSTree {
         int rsearch(int val, Node *next);
         void rinsert(int d, Node *head, Node *trailer);
         int treesum(Node *next);
-        Node* get_leaf(Node* n);
+        Node* get_leaf(Node *n);
+        int count_leaves(Node *n);
+        int get_height(Node *n, int height);
 
     public:
         BSTree();
@@ -20,4 +22,10 @@ class BSTree {
         int search(int value);
         int treesum();
         void remove(int i); 
+        int count_leaves();
+
+        // One based counting, starts from 1
+        int get_height();
+
+        int sum_of_given_level(int level);
 };

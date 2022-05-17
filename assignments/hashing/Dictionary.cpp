770cbdcb7ab49267eb59f12e3fada5119f308b72
xiaogavin-hunter <gavin.xiao89@myhunter.cuny.edu>
2022-05-17 09:53:41 -0400

    added code for getkeys

--- assignments/hashing/Dictionary.cpp
+++ assignments/hashing/Dictionary.cpp
@@ -14,7 +14,6 @@ int Dictionary::hash(Person *p) {
 
 void Dictionary::enter(Person *p) {
     table[this->hash(p)]->insert(p);
-
     return;
 }
 
@@ -22,4 +21,34 @@ void Dictionary::enter(std::string first, std::string last, int id) {
     Person *p = new Person(first, last, id);
     this->enter(p);
     return;
+}
+
+Person* Dictionary::retrieve(std::string first, std::string last) {
+    Person *data = new Person(first, last, 0);
+    int hash_number = hash(data);
+    Node *walker = table[hash_number]->getHead();
+
+    while(walker) {
+        if(walker->getPerson()->get_name() == data->get_name()) {
+            return walker->getPerson();
+        }
+
+        walker = walker->getNext();
+    }
+
+    return nullptr;
+}
+
+std::string Dictionary::getKeys() {
+    std::string keys = ""; 
+    for(int i = 0; i < 8; i++) {
+        Node *tmp = table[i]->getHead();
+
+        while(tmp) { 
+            keys += tmp->getData() + " | ";
+            tmp = tmp->getNext();
+        }
+    }
+
+    return keys;
 }

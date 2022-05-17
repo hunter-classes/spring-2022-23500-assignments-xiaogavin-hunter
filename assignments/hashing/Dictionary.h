a58adc39ca2df008117eb15b24d671f48dde7735
xiaogavin-hunter <gavin.xiao89@myhunter.cuny.edu>
2022-05-17 10:09:33 -0400

    added deconstructor

--- assignments/hashing/Dictionary.h
+++ assignments/hashing/Dictionary.h
@@ -8,12 +8,11 @@ class Dictionary{
 
     public:
         Dictionary();
+        ~Dictionary();
 
         int hash(Person *p);
         void enter(Person *data);
         void enter(std::string first, std::string last, int num);
         Person* retrieve(std::string first, std::string last);
         std::string getKeys();
-
-    // ~Dictionary();
 };

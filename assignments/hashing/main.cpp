5ed66a7de7ef2c1813b0256ce11c332959fd3334
xiaogavin-hunter <gavin.xiao89@myhunter.cuny.edu>
2022-05-17 09:53:46 -0400

    added code

--- assignments/hashing/main.cpp
+++ assignments/hashing/main.cpp
@@ -2,7 +2,17 @@
 #include "Dictionary.h"
 
 int main() {
+    Dictionary *dict = new Dictionary();
+    Person *one = new Person("John", "Smith", 1008484);
+    Person *two = new Person("Mary", "Gray", 1008666);
+    Person *three = new Person("George", "Brownings", 14884442);
+    dict->enter(one);
+    dict->enter(two);
+    dict->enter(three);
 
+    std::cout << dict->getKeys() << std::endl;
+
+    // Brownings, George| \n\nGray, Mary| \n\n\n\nSmith, John\n
 
     return 0;
 }
